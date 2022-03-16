00c8a089a614c940d23a5bd573e65f6b68cf94e7
Ricardo Valadez <99301599+Ricardo-Valadez@users.noreply.github.com>
2022-03-16 12:04:59 -0700

    Add files via upload

---
+++ Ejercicio73.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+/*
+*
+* Problema 73
+*
+* Introducir un entero N y determinar si se trata de un cuadrado perfecto.
+*
+*/
+
+//@Author   Ricardo Valadez Leal
+//@Date     6/3/22
+
+//Metodo para verificar si un numero es cuadrado perfecto
+ bool EsCuadradoPerfecto(int x){
+    bool resultado =false;
+    int i;
+    
+        for(i=1;i<x;i++){
+            if(i*i==x){
+                resultado=true;
+                break;
+            }
+        }
+    return resultado;
+}
+
+int main (){ 
+    int n,i;
+    bool cuadrado=false;
+    
+    cout<< "Dame un numero positivo ";
+    cin >> n;
+
+    //Realizamos la pregunta y mandamos a llamar el metodo de cuadrado perfecto
+    while (n>0){ 
+        cuadrado=EsCuadradoPerfecto(n);
+        
+        //Dependiendo del resultado arrojamos la respuesta apropiada
+        if(cuadrado){
+            cout << n << " Es cuadrado perfecto\n";
+        }
+        else{
+            cout << n << " No es un cuadrado perfecto\n";
+        }
+        cout<< "Dame un numero positivo para continuar\n" <<"O pon un 0 para terminar ejecucion :" ;
+        cin >> n;
+    }
+
+}
+
