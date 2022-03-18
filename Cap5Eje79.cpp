6a3ca3c8bb5f10e4b8950c414d6d1bca5a9c4b5c
Ricardo Valadez <99301599+Ricardo-Valadez@users.noreply.github.com>
2022-03-18 15:20:11 -0700

    Add files via upload

---
+++ Cap5Eje79.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/*
+*
+* 79. Encontrar el promedio de 1,000 números tomados al azar
+*
+*/
+
+//@Author   Ricardo Valadez Leal
+//@Date     17/3/22
+
+
+//Metodo para generar numeros al azar
+int azar(void){
+    
+    int numeroalt;
+    numeroalt= 1+rand();
+    
+    return numeroalt;
+}
+
+int main()
+{
+    int suma,numazar,promedio;
+
+    //Necesario para ir sumando los numeros
+    for (int i=1;i<=1000;i++){
+
+        numazar=azar();
+
+        suma=suma+numazar;
+    } 
+    //Operacion para sacar el promedio
+    promedio= suma /1000;
+
+    cout <<"Suma total: "<< suma << " Promedio final : " << promedio <<endl;
+}
---
+++ Cap5Eje80.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/*
+*
+* 80. La clase de ecología tiene cinco estudiantes que obtuvieron las siguientes notas en el examen: 
+* 75, 93, 41, 98 y 71. El profesor desea calculare el promedio de las notas. Escribir un programa para realizar el cálculo.
+*/
+
+//@Author   Ricardo Valadez Leal
+//@Date     17/3/22
+
+int main()
+{
+    double promedio,suma;
+
+    //Suma de los numero para sacar el promedio
+    suma= 75+93+41+98+71;
+    promedio= suma / 5;
+
+    cout << "Calificaciones de un examen de cinco alumnos {75, 93, 41, 98, 71}" <<endl;
+    cout <<"Promedio final : " << promedio <<endl;
+}
